993373f844420a2cc14213d17dd931ea607693c6
jesakim <jesakim1@gmail.com>
2023-03-16 01:28:07 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
+int main()
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
