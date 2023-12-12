eebef51c306a408357441650ab7223968f8cbd82
Jamal Eddine Sakim <112765925+jesakim@users.noreply.github.com>
2023-12-12 16:53:39 +0100

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,19 +1,46 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - searches for a value in a sorted skip list of integers.
- * @list: pointer to the head of the skip list to search in.
- * @value: value to search for.
- * Return: pointer to the first node where value is located.
- * resturns NULL if value is not present in the list or if head is NULL.
- **/
+ * linear_skip - skippity skip search singular linked list
+ * @list: pointer to head node
+ * @value: value to search for
+ *
+ * Return: the node found or NULL
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
+	skiplist_t *last = list;
+
 	if (!list)
-		return (list);
+		return (NULL);
 
-	/* It's not what you expected, I still have to do this task */
-	value = value;
-
-	return (list);
+	while (list->n < value)
+	{
+		if (!list->express)
+			break;
+		last = list;
+		list = list->express;
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+	}
+	if (list->n >= value)
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				last->index, list->index);
+		list = last;
+	}
+	else
+	{
+		for (last = list; last->next; last = last->next)
+			;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				list->index, last->index);
+	}
+	while (list)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
 }
